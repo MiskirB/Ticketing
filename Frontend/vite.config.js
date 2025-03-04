5a3f8cd79cd51205ea4b2dbab2dd73ae70a2c275
MiskirB <miskerbab@gmail.com>
2025-03-05 02:27:38 +0300

    c

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:5000",
-        target: "https://ticketing-jlk8.onrender.com",
+        target: "http://localhost:5000",
+        // target: "https://ticketing-jlk8.onrender.com",
         secure: true,
       },
     },
