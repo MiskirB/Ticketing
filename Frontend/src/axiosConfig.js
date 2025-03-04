af36a91a0f932284b19c4db1a64c2f54b8bc118d
MiskirB <miskerbab@gmail.com>
2025-03-05 02:50:07 +0300

    comp

--- Frontend/src/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosBase = axios.create({
-  baseURL: "https://ticketing-jlk8.onrender.com/api",
-  // baseURL: "http://localhost:5500/api",
-});
-export default axiosBase;
